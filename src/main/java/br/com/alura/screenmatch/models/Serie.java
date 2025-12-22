c54533e394da45854cb395797ef34f1604d6298a
Luiz <luizzlh2004@gmail.com>
2025-12-21 22:29:51 -0300

    Adicionando carregamento completo dos itens para carregamento efetivo e mais rápido e adicionando a chave estrangeira dos itens.

--- src/main/java/br/com/alura/screenmatch/models/Serie.java
+++ src/main/java/br/com/alura/screenmatch/models/Serie.java
@@ -24,7 +24,7 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie(DadosSerie dadosSerie){
@@ -54,6 +54,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(episodio -> episodio.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -121,6 +122,7 @@ public class Serie {
                 ", avaliacao=" + avaliacao +
                 ", atores='" + atores + '\'' +
                 ", poster='" + poster + '\'' +
-                ", sinopse='" + sinopse + '\'';
+                ", sinopse='" + sinopse + '\'' +
+                ", episodios='" + episodios + '\'';
     }
 }
