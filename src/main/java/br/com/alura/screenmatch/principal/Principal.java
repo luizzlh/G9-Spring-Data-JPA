2f731a766ac9e22b3a3ba1f12a158e130404b0eb
Luiz <luizzlh2004@gmail.com>
2025-12-25 18:22:25 -0300

    Adicionando multiplos critérios para localização no banco.

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -33,6 +33,7 @@ public class Principal {
                     2 - Buscar episódios
                     3 - Listar séries buscadas
                     4 - Buscar série por título
+                    5 - Buscar séries pelo Ator
                     
                     0 - Sair                                 
                     """;
@@ -53,6 +54,9 @@ public class Principal {
                     break;
                 case 4:
                     buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -125,6 +129,15 @@ public class Principal {
         }else{
             System.out.println("Série não encontrada!");
         }
+    }
 
+    private void buscarSeriesPorAtor() {
+        System.out.println("Qual o nome para busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+        seriesEncontradas.forEach( serie -> System.out.println(serie.getTitulo() + " avaliação: " + serie.getAvaliacao()));
     }
 }
